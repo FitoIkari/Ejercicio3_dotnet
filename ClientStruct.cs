0c5572db05b7968ec5e2b28836fd3f7d688ca39a
FitoIkari <chris_fito@hotmail.com>
2023-02-01 18:16:18 -0600

    first commit

---
+++ ClientStruct.cs
@@ -0,0 +1,22 @@
+﻿public struct Client {
+    public string FullName { get; }
+    public string Phone { get; }
+    public string Address { get; }
+    public string Email { get; }
+    public bool NewClient { get; }
+    public Client(string fullName, string phone, string address, string email, bool newClient) {
+        FullName = fullName;
+        Phone = phone;
+        Address = address;
+        Email = email;
+        NewClient = newClient;
+    }
+
+    public override string ToString() {
+        return $"El usuario {FullName} {(NewClient ? " " : "no")} es un cliente nuevo.\n" +
+            $"A continuación se muestra la información del usuario: \n" +
+            $"- Teléfono: {Phone}\n" +
+            $"- Dirección: {Address}\n" +
+            $"- Email: {Email}\n";
+    }
+}
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Ejercicios sesión 3 - Curso de C# Open Bootcamp
+
+Este repositorio es contiene los ejercicios de la segunda sesión del curso de C# de [open bootcamp](https://open-bootcamp.com/)
+
+## Enunciado del ejercicio:
+
+### Ejercicio 1
+
+Crea una estructura de datos para un cliente con estos campos:
+
+- Nombre completo
+
+- Teléfono
+
+- Dirección
+
+- Email
+
+- Si es nuevo cliente
+
+**Bonus:** escribe un método para presentar estos datos desde la estructura al hacer Console.WriteLine(...)
